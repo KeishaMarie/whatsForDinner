738fa687afaeb86b015b0830388726e9b6546f84
Keisha Hepner <kmhepner90@gmail.com>
2022-09-07 16:55:26 -0700

    WIP Print elements for ingredients
    Co-authored-by: Ted Gold <ted.jonathan.gold@gmail.com>

--- src/index.js
+++ src/index.js
@@ -29,8 +29,31 @@ function printError(request, apiResponse) {
 }
 
 function printElements(apiResponse) { 
-  console.log(apiResponse.meals[0])
-  document.querySelector('#showResponse').innerText = `Make ${apiResponse.meals[0].strMeal} for your fam bam tonight!`; 
+  // let measurementArray = [apiResponse.meals[0].strMeasure1, apiResponse.meals[0].strMeasure2, apiResponse.meals[0].strMeasure3, apiResponse.meals[0].strMeasure4, apiResponse.meals[0].strMeasure5, apiResponse.meals[0].strMeasure6, apiResponse.meals[0].strMeasure7, apiResponse.meals[0].strMeasure8, apiResponse.meals[0].strMeasure9, apiResponse.meals[0].strMeasure10, apiResponse.meals[0].strMeasure11, apiResponse.meals[0].strMeasure12, apiResponse.meals[0].strMeasure13, apiResponse.meals[0].strMeasure14, apiResponse.meals[0].strMeasure15, apiResponse.meals[0].strMeasure16, apiResponse.meals[0].strMeasure17, apiResponse.meals[0].strMeasure18, apiResponse.meals[0].strMeasure19, apiResponse.meals[0].strMeasure20];
+  // let ingredientArray = [apiResponse.meals[0].strIngredient1, apiResponse.meals[0].strIngredient2, apiResponse.meals[0].strIngredient3, apiResponse.meals[0].strIngredient4, apiResponse.meals[0].strIngredient5, apiResponse.meals[0].strIngredient6, apiResponse.meals[0].strIngredient7, apiResponse.meals[0].strIngredient8, apiResponse.meals[0].strIngredient9, apiResponse.meals[0].strIngredient10, apiResponse.meals[0].strIngredient11, apiResponse.meals[0].strIngredient12, apiResponse.meals[0].strIngredient13, apiResponse.meals[0].strIngredient14, apiResponse.meals[0].strIngredient15, apiResponse.meals[0].strIngredient16, apiResponse.meals[0].strIngredient17, apiResponse.meals[0].strIngredient18, apiResponse.meals[0].strIngredient19, apiResponse.meals[0].strIngredient20];
+
+  let tryArray = [apiResponse.meals[0].strMeasure1 + apiResponse.meals[0].strIngredient1, '\n', apiResponse.meals[0].strMeasure2 + apiResponse.meals[0].strIngredient2, '\n', apiResponse.meals[0].strMeasure3 + apiResponse.meals[0].strIngredient3, '\n', apiResponse.meals[0].strMeasure4 + apiResponse.meals[0].strIngredient4, '\n', apiResponse.meals[0].strMeasure5 + apiResponse.meals[0].strIngredient5, '\n', apiResponse.meals[0].strMeasure6 + apiResponse.meals[0].strIngredient6, '\n', apiResponse.meals[0].strMeasure7 + apiResponse.meals[0].strIngredient7, '\n', apiResponse.meals[0].strMeasure8 + apiResponse.meals[0].strIngredient8, '\n', apiResponse.meals[0].strMeasure9 + apiResponse.meals[0].strIngredient9, '\n', apiResponse.meals[0].strMeasure10 + apiResponse.meals[0].strIngredient10, '\n', apiResponse.meals[0].strMeasure11 + apiResponse.meals[0].strIngredient11, '\n', apiResponse.meals[0].strMeasure12 + apiResponse.meals[0].strIngredient12, '\n', apiResponse.meals[0].strMeasure13 + apiResponse.meals[0].strIngredient13, '\n', apiResponse.meals[0].strMeasure14 + apiResponse.meals[0].strIngredient14, '\n', apiResponse.meals[0].strMeasure15 + apiResponse.meals[0].strIngredient15, '\n', apiResponse.meals[0].strMeasure16 + apiResponse.meals[0].strIngredient16, '\n', apiResponse.meals[0].strMeasure17 + apiResponse.meals[0].strIngredient17, '\n' , apiResponse.meals[0].strMeasure18 + apiResponse.meals[0].strIngredient18, '\n', apiResponse.meals[0].strMeasure19 + apiResponse.meals[0].strIngredient19, '\n', apiResponse.meals[0].strMeasure20 + apiResponse.meals[0].strIngredient20];
+
+  let joinIngredients = tryArray.concat(",");
+  
+  // let ingredientMeasures = [];
+
+  // let l = Math.min(measurementArray.length, ingredientArray.length);
+
+  // for(let i = 0; i< l; i++){
+
+  //   ingredientMeasures.push(measurementArray[i] + " - " + ingredientArray[i],'\n');
+  // }
+
+  document.querySelector('#showMealName').innerText = `Make ${apiResponse.meals[0].strMeal} for your fam bam tonight!`;
+  
+  document.querySelector('#showIngredients').innerText = `Grab these ingredients from yo kitchen:
+
+  ${joinIngredients}`;
+
+  document.querySelector('#showInstructions').innerText = `Follow these instructions:
+
+  ${apiResponse.meals[0].strInstructions}`; 
 }
 
 function handleFormSubmission(event) {
