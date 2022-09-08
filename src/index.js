a45dc59142c20e319462b523a8fab414d872c947
Keisha Hepner <kmhepner90@gmail.com>
2022-09-08 13:14:04 -0700

    WIP this API may not be the best option for what we are trying to accomplish. Switching projects
    Co-authored-by: Ted Gold <ted.jonathan.gold@gmail.com>

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ import './css/styles.css';
 
 function getRandomRecipe() {
   let request = new XMLHttpRequest();
-  const url = `https://www.themealdb.com/api/json/v1/1/random.php`;
+  let url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=`;
+    // if (document.querySelector("#mainIngredient") !== null) {
+    //   printElements (url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=`);
+    // } else {
+    //   printElements (url = `https://www.themealdb.com/api/json/v1/1/random.php`);
+    
 
   request.addEventListener("loadend", function() {
     const response = JSON.parse(this.responseText);
@@ -16,7 +21,7 @@ function getRandomRecipe() {
     } else {
       printError(this, response);
     }
-  });
+  }); 
 
   request.open("GET", url, true);
   request.send();
@@ -31,25 +36,25 @@ function printError(request, apiResponse) {
 function printElements(apiResponse) { 
   // let measurementArray = [apiResponse.meals[0].strMeasure1, apiResponse.meals[0].strMeasure2, apiResponse.meals[0].strMeasure3, apiResponse.meals[0].strMeasure4, apiResponse.meals[0].strMeasure5, apiResponse.meals[0].strMeasure6, apiResponse.meals[0].strMeasure7, apiResponse.meals[0].strMeasure8, apiResponse.meals[0].strMeasure9, apiResponse.meals[0].strMeasure10, apiResponse.meals[0].strMeasure11, apiResponse.meals[0].strMeasure12, apiResponse.meals[0].strMeasure13, apiResponse.meals[0].strMeasure14, apiResponse.meals[0].strMeasure15, apiResponse.meals[0].strMeasure16, apiResponse.meals[0].strMeasure17, apiResponse.meals[0].strMeasure18, apiResponse.meals[0].strMeasure19, apiResponse.meals[0].strMeasure20];
   // let ingredientArray = [apiResponse.meals[0].strIngredient1, apiResponse.meals[0].strIngredient2, apiResponse.meals[0].strIngredient3, apiResponse.meals[0].strIngredient4, apiResponse.meals[0].strIngredient5, apiResponse.meals[0].strIngredient6, apiResponse.meals[0].strIngredient7, apiResponse.meals[0].strIngredient8, apiResponse.meals[0].strIngredient9, apiResponse.meals[0].strIngredient10, apiResponse.meals[0].strIngredient11, apiResponse.meals[0].strIngredient12, apiResponse.meals[0].strIngredient13, apiResponse.meals[0].strIngredient14, apiResponse.meals[0].strIngredient15, apiResponse.meals[0].strIngredient16, apiResponse.meals[0].strIngredient17, apiResponse.meals[0].strIngredient18, apiResponse.meals[0].strIngredient19, apiResponse.meals[0].strIngredient20];
+  // let combinedArray = [];
+  // let showIngredients = document.querySelector('#showIngredients');
 
-  let tryArray = [apiResponse.meals[0].strMeasure1 + apiResponse.meals[0].strIngredient1, '\n', apiResponse.meals[0].strMeasure2 + apiResponse.meals[0].strIngredient2, '\n', apiResponse.meals[0].strMeasure3 + apiResponse.meals[0].strIngredient3, '\n', apiResponse.meals[0].strMeasure4 + apiResponse.meals[0].strIngredient4, '\n', apiResponse.meals[0].strMeasure5 + apiResponse.meals[0].strIngredient5, '\n', apiResponse.meals[0].strMeasure6 + apiResponse.meals[0].strIngredient6, '\n', apiResponse.meals[0].strMeasure7 + apiResponse.meals[0].strIngredient7, '\n', apiResponse.meals[0].strMeasure8 + apiResponse.meals[0].strIngredient8, '\n', apiResponse.meals[0].strMeasure9 + apiResponse.meals[0].strIngredient9, '\n', apiResponse.meals[0].strMeasure10 + apiResponse.meals[0].strIngredient10, '\n', apiResponse.meals[0].strMeasure11 + apiResponse.meals[0].strIngredient11, '\n', apiResponse.meals[0].strMeasure12 + apiResponse.meals[0].strIngredient12, '\n', apiResponse.meals[0].strMeasure13 + apiResponse.meals[0].strIngredient13, '\n', apiResponse.meals[0].strMeasure14 + apiResponse.meals[0].strIngredient14, '\n', apiResponse.meals[0].strMeasure15 + apiResponse.meals[0].strIngredient15, '\n', apiResponse.meals[0].strMeasure16 + apiResponse.meals[0].strIngredient16, '\n', apiResponse.meals[0].strMeasure17 + apiResponse.meals[0].strIngredient17, '\n' , apiResponse.meals[0].strMeasure18 + apiResponse.meals[0].strIngredient18, '\n', apiResponse.meals[0].strMeasure19 + apiResponse.meals[0].strIngredient19, '\n', apiResponse.meals[0].strMeasure20 + apiResponse.meals[0].strIngredient20];
-
-  let joinIngredients = tryArray.concat(",");
-  
-  // let ingredientMeasures = [];
-
-  // let l = Math.min(measurementArray.length, ingredientArray.length);
-
-  // for(let i = 0; i< l; i++){
-
-  //   ingredientMeasures.push(measurementArray[i] + " - " + ingredientArray[i],'\n');
+  // for (let i = 0; i < measurementArray.length; i += 1) {
+  //   if ((!(measurementArray.includes(null) || ingredientArray.includes(null))) || (!(measurementArray.includes(' ') || ingredientArray.includes(' ')))) {
+  //   combinedArray.push(measurementArray[i] + " " + ingredientArray[i]);
+  //   }
   // }
+  // console.log(combinedArray);
+  // document.querySelector('#showIngredients').append(combinedArray);
+  // let tryArray = [apiResponse.meals[0].strMeasure1 + apiResponse.meals[0].strIngredient1, '\n', apiResponse.meals[0].strMeasure2 + apiResponse.meals[0].strIngredient2, '\n', apiResponse.meals[0].strMeasure3 + apiResponse.meals[0].strIngredient3, '\n', apiResponse.meals[0].strMeasure4 + apiResponse.meals[0].strIngredient4, '\n', apiResponse.meals[0].strMeasure5 + apiResponse.meals[0].strIngredient5, '\n', apiResponse.meals[0].strMeasure6 + apiResponse.meals[0].strIngredient6, '\n', apiResponse.meals[0].strMeasure7 + apiResponse.meals[0].strIngredient7, '\n', apiResponse.meals[0].strMeasure8 + apiResponse.meals[0].strIngredient8, '\n', apiResponse.meals[0].strMeasure9 + apiResponse.meals[0].strIngredient9, '\n', apiResponse.meals[0].strMeasure10 + apiResponse.meals[0].strIngredient10, '\n', apiResponse.meals[0].strMeasure11 + apiResponse.meals[0].strIngredient11, '\n', apiResponse.meals[0].strMeasure12 + apiResponse.meals[0].strIngredient12, '\n', apiResponse.meals[0].strMeasure13 + apiResponse.meals[0].strIngredient13, '\n', apiResponse.meals[0].strMeasure14 + apiResponse.meals[0].strIngredient14, '\n', apiResponse.meals[0].strMeasure15 + apiResponse.meals[0].strIngredient15, '\n', apiResponse.meals[0].strMeasure16 + apiResponse.meals[0].strIngredient16, '\n', apiResponse.meals[0].strMeasure17 + apiResponse.meals[0].strIngredient17, '\n', apiResponse.meals[0].strMeasure18 + apiResponse.meals[0].strIngredient18, '\n', apiResponse.meals[0].strMeasure19 + apiResponse.meals[0].strIngredient19, '\n', apiResponse.meals[0].strMeasure20 + apiResponse.meals[0].strIngredient20];
+
+
 
   document.querySelector('#showMealName').innerText = `Make ${apiResponse.meals[0].strMeal} for your fam bam tonight!`;
   
-  document.querySelector('#showIngredients').innerText = `Grab these ingredients from yo kitchen:
+  // document.querySelector('#showIngredients').innerText = `Grab these ingredients from yo kitchen:
 
-  ${joinIngredients}`;
+  // // ${tryArray}`;
 
   document.querySelector('#showInstructions').innerText = `Follow these instructions:
 
@@ -58,6 +63,7 @@ function printElements(apiResponse) {
 
 function handleFormSubmission(event) {
   event.preventDefault();
+  document.querySelector("#showIngredients").innerText = null;
   getRandomRecipe();
 }
 
